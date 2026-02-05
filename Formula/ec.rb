e36d0d9c7a1654881c0cc518a50b2e218d477f92
neo <chojs990222@gmail.com>
2026-02-05 13:11:22 +0900

    init

---
+++ Formula/ec.rb
@@ -0,0 +1,25 @@
+class Ec < Formula
+  desc "TUI native Git mergetool with 3 pane"
+  homepage "https://github.com/chojs23/ec"
+  url "https://github.com/chojs23/ec/archive/refs/tags/v0.1.3.tar.gz"
+  sha256 "c4cca328cc9d181f168ea6d95caa649e1f05b5c750b78de22f9e17488ccb40c3"
+  license "MIT"
+
+  depends_on "go" => :build
+  def install
+    ldflags = "-s -w -X main.version=#{version}"
+    system "go", "build", *std_go_args(ldflags: ldflags), "./cmd/ec"
+  end
+
+  test do
+    (testpath/"merged.txt").write <<~EOS
+      line1
+      <<<<<<< HEAD
+      ours
+      =======
+      theirs
+      >>>>>>> branch
+    EOS
+    shell_output("#{bin}/ec --check --merged #{testpath}/merged.txt", 1)
+  end
+end
